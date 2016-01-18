91f7a0032665eacbf5e1786bed5b981db2adf17f
bbofrk <mingliwanglau@gmail.com>
2016-01-19 08:49:19 +1300

    add collections

--- App.jsx
+++ App.jsx
@@ -1,5 +1,15 @@
 // App component - represents the whole app
 App = React.createClass({
+  mixins: [ReactMeteorData],
+
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}).fetch()
+    }
+  },
+
+
+
   getTasks() {
     return [
       { _id: 1, text: "This is task 1" },
@@ -9,7 +19,7 @@ App = React.createClass({
   },
 
   renderTasks() {
-    return this.getTasks().map((task) => {
+    return this.data.tasks.map((task) => {
       return <Task key={task._id} task={task} />;
     });
   },
