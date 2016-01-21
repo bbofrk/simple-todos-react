ae36e396669561d2010f795665ff3ff1e2feb2f0
bbofrk <mingliwanglau@gmail.com>
2016-01-21 15:42:53 +1300

    add input

--- App.jsx
+++ App.jsx
@@ -24,11 +24,38 @@ App = React.createClass({
     });
   },
 
+  handleSubmit(event) {
+    event.preventDefault();
+
+    //find the text field via the React Ref
+    var text = React.findDOMNode(this.refs.textInput).value.trim();
+    Tasks.insert({
+      text: text,
+      createdAt: new Date() //current time
+    })
+
+    //clear form
+    React.findDOMNode(this.refs.textInput).value = "";
+  },
+
+  getMeteorData() {
+    return {
+      tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+    }
+  },
+
   render() {
     return (
       <div className="container">
         <header>
           <h1>Todo List</h1>
+          <form action="" className="new-task" onSubmit={this.handleSubmit}>
+            <input
+              type="text"
+              ref="textInput"
+              placeholder="Type to add new tasks"
+            />
+          </form>
         </header>
 
         <ul>
